8df9d39521c40b93f0861be8442f41e5f1268d53
Luis Maldonado <luis.maldonado@atos.net>
2025-04-17 14:41:17 +0200

    first version

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from fastapi import FastAPI, Request, HTTPException,Form
 from fastapi.middleware.cors import CORSMiddleware
 import requests
 import logging
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
 logger = logging.getLogger("sso-app")
 
 origins = [
     "http://localhost:8080",  # or whatever your frontend runs on
     "https://wordpress-bakuriani-tst.mth.mev.atos.net",
+    "https://wordpress-sporteurope-tst.mth.mev.atos.net",  
+    "https://wordpress-skopje-tst.mth.mev.atos.net",     
 ]
 
 app.add_middleware(
@@ -21,33 +24,29 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-# --- CONFIGURE THESE VALUES ---
-KEYCLOAK_BASE_URL = "https://keycloak-tst.mth.mev.atos.net"
-REALM = "EOC"
-CLIENT_ID = "myclient"
-CLIENT_SECRET = "mysecret"
-REDIRECT_URI = "http://localhost:8000/callback"
-# ------------------------------
-
-TOKEN_URL = f"{KEYCLOAK_BASE_URL}/realms/{REALM}/protocol/openid-connect/token"
 
 
+CLIENT_SECRET = os.getenv("KEYCLOAK_CLIENT_SECRET")
+KEYCLOAK_TOKEN_URL = os.getenv("KEYCLOAK_TOKEN_URL")
+
+logger.info("CLIENT_SECRET %s", CLIENT_SECRET)
+logger.info("TOKEN_URL: %s", KEYCLOAK_TOKEN_URL)
 
 @app.post("/token")
 async def token(grant_type: str = Form(...),code: str = Form(...),redirect_uri: str = Form(...),client_id: str = Form(...)
 ):
       
-    logger.info("Sending request to %s", TOKEN_URL)
+    logger.info("Sending request to %s", KEYCLOAK_TOKEN_URL)
     
     data = {
         "grant_type": grant_type,
         "code": code,
         "client_id": client_id,
-     #   "client_secret": CLIENT_SECRET,
+        "client_secret": CLIENT_SECRET,
         "redirect_uri": redirect_uri,
     }
     
-    response = requests.post(TOKEN_URL, data=data)
+    response = requests.post(KEYCLOAK_TOKEN_URL, data=data)
 
     if response.status_code != 200:
       raise HTTPException(status_code=401, detail="Failed to exchange code for token")
